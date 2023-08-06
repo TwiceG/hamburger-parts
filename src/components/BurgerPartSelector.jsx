4856d0f5fa7187b6c59c7c9bb4a71b4d71412623
TwiceG <g0stron0g@gmail.com>
2023-08-06 16:51:53 +0200

    remake the selector to be able to choose more part

--- src/components/BurgerPartSelector.jsx
+++ src/components/BurgerPartSelector.jsx
@@ -1,47 +1,92 @@
-import React from 'react';
-
 export const BurgerPartSelector = ({ parts, selectedParts, setSelectedParts }) => {
-  const handleChange = (event) => {
-    const itemValue = event.target.value;
-    let updatedItems;
-
-    if (event.target.checked) {
-      if (!selectedParts.includes(itemValue)) {
-        // Ensure hamburger-puff-top is always the first item
-        if (itemValue === 'hamburger-puff-top') {
-          updatedItems = ['hamburger-puff-top', ...selectedParts];
-        } else {
-          updatedItems = [...selectedParts, itemValue];
+    const handleChange = (event, part) => {
+      const newCount = parseInt(event.target.value, 10);
+      const updatedParts = [...selectedParts];
+      const currentCount = countPart(part);
+  
+      if (part === 'hamburger-puff-top') {
+        if (newCount > currentCount) {
+          // Add "hamburger-puff-top" to the array at the beginning (up to the new count)
+          for (let i = 0; i < Math.min(newCount - currentCount, 1); i++) {
+            updatedParts.unshift(part);
+          }
+        } else if (newCount < currentCount) {
+          // Remove "hamburger-puff-top" from the array (up to the new count)
+          for (let i = 0; i < Math.abs(newCount - currentCount); i++) {
+            const index = updatedParts.indexOf(part);
+            if (index !== -1) {
+              updatedParts.splice(index, 1);
+            }
+          }
+        }
+    } else if (part === 'hamburger-puff-bottom') {
+        if (newCount > currentCount && currentCount < 2) {
+          // Add "hamburger-puff-bottom" to the array at the end (up to the new count and max count 2)
+          for (let i = 0; i < Math.min(newCount - currentCount, 2 - currentCount); i++) {
+            updatedParts.push(part);
+          }
+        } else if (newCount < currentCount) {
+          // Remove "hamburger-puff-bottom" from the array (up to the new count)
+          for (let i = 0; i < Math.abs(newCount - currentCount); i++) {
+            const index = updatedParts.lastIndexOf(part);
+            if (index !== -1) {
+              updatedParts.splice(index, 1);
+            }
+          }
+        }
+      } else {
+        // Handle other parts
+        if (newCount > currentCount) {
+          // Add part to the array multiple times to reach the new count
+          for (let i = 0; i < newCount - currentCount; i++) {
+            updatedParts.push(part);
+          }
+        } else if (newCount < currentCount) {
+          // Remove part from the array multiple times to reach the new count
+          for (let i = 0; i < currentCount - newCount; i++) {
+            const index = updatedParts.indexOf(part);
+            if (index !== -1) {
+              updatedParts.splice(index, 1);
+            }
+          }
         }
       }
-    } else {
-      // Remove the item from the array
-      updatedItems = selectedParts.filter((item) => item !== itemValue);
-    }
-
-    // Ensure hamburger-puff-bottom is always the last item
-    if (updatedItems.includes('hamburger-puff-bottom')) {
-      updatedItems = updatedItems.filter((item) => item !== 'hamburger-puff-bottom');
-      updatedItems.push('hamburger-puff-bottom');
-    }
-
-    setSelectedParts(updatedItems);
+  
+      // Ensure "hamburger-puff-bottom" is always at the end
+      const puffBottomIndex = updatedParts.indexOf('hamburger-puff-bottom');
+      if (puffBottomIndex !== -1) {
+        updatedParts.splice(puffBottomIndex, 1);
+      }
+      updatedParts.push('hamburger-puff-bottom');
+  
+      setSelectedParts(updatedParts);
+    };
+  
+    const countPart = (part) => {
+      let count = 0;
+      for (let i = 0; i < selectedParts.length; i++) {
+        if (selectedParts[i] === part) {
+          count++;
+        }
+      }
+      return count;
+    };
+  
+    return (
+      <div>
+        {parts.map((part, index) => (
+          <div key={index}>
+            <input
+              type="number"
+              min={0}
+              max={2}
+              value={countPart(part)}
+              onChange={(event) => handleChange(event, part)}
+            />
+            <label htmlFor={index}>{part}</label>
+          </div>
+        ))}
+      </div>
+    );
   };
-
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <div key={index}>
-          <input
-            type="checkbox"
-            onChange={handleChange}
-            value={part}
-            id={index}
-            checked={selectedParts.includes(part)}
-          />
-          <label htmlFor={index}>{part}</label>
-        </div>
-      ))}
-    </div>
-  );
-};
+  
